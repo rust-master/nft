3d96aaaa57c4803424f06b00bfce3a788918c1e5
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-09-24 20:45:03 +0500

    frontend added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.createEmoji = this.createEmoji.bind(this);
+    this.state = {
+      emojiUnicode: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      emojiUnicode: event.target.value,
+    });
+  };
+
+  createEmoji = () => {
+    const { emojiUnicode } = this.state;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          name="emojiUnicode"
+          type="text"
+          placeholder="Enter Unicode Emoji"
+          onChange={this.handleChange}
+        />
+
+        <button onClick={this.createEmoji}>Submit</button>
+        <h5>{this.state.emojiUnicode}</h5>
+      </div>
+    );
+  }
 }
 
 export default App;
